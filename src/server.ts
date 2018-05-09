63f56f9c1c9c7ff78855fcaddf507cfed41e7256
Bailey Everts <beverts@levelsbeyond.com>
2018-05-09 12:52:04 -0600

    feat(upload): add ability to upload files over the api

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
+import * as fileUpload from 'express-fileupload';
 import { SoundPlayer } from './main';
+import { config } from './config';
 
 const app = express();
 const player = new SoundPlayer();
@@ -7,15 +9,17 @@ const player = new SoundPlayer();
 app.set('json spaces', 10);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 });
 
 app.use((req, res, next) => {
     console.log(`${(new Date()).toLocaleString()} ${req.socket.remoteAddress.replace('::ffff:', '')} ${req.method} ${req.url}`);
     next();
 });
+
+app.use(fileUpload());
 
 app.get('/api/sounds', (req, res) => {
     player.getAvailableSounds().then(sounds => res.send(sounds)).catch(err => res.status(500).send('An error occured'));
@@ -32,5 +36,22 @@ app.get('/api/sounds/:filename', (req, res) => {
         }
     });
 });
+
+app.post('/api/upload', (req, res) => {
+    if (!req.files) {
+        res.status(400).send('No files were uploaded.');
+    } else {
+        let sampleFile = <fileUpload.UploadedFile>req.files.file;
+        sampleFile.mv(`${config.soundDirectory}/${sampleFile.name}`, (err) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send(err)
+            } else {
+                res.send('File uploaded!');
+            }
+        });
+    }
+});
+
 
 app.listen(3000);
